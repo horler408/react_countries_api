96d2dd0c49bd3d88a5519eb2ae413a27ba6b396e
horler408 <horler.web@gmail.com>
2020-09-22 22:55:45 +0100

    Updates

---
+++ precache-manifest.4519f25447733080507f8e46cd86a766.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f0b66dfd9e23e44b1d211f331b22e6d",
+    "url": "/github.io/react_countries_api/index.html"
+  },
+  {
+    "revision": "6b86ae4e235cfad49a69",
+    "url": "/github.io/react_countries_api/static/css/main.64ce2f62.chunk.css"
+  },
+  {
+    "revision": "f340d8fd36bfd9d62846",
+    "url": "/github.io/react_countries_api/static/js/2.54986c46.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github.io/react_countries_api/static/js/2.54986c46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b86ae4e235cfad49a69",
+    "url": "/github.io/react_countries_api/static/js/main.dc664269.chunk.js"
+  },
+  {
+    "revision": "e703afb97794842426c9",
+    "url": "/github.io/react_countries_api/static/js/runtime-main.620c915f.js"
+  }
+]);
